827c93f5367452d02cf53d0ce1ca1957fcc6c33e
judy teng <judy0006978kimo@hotmail.com>
2019-04-14 10:31:36 +0800

    add file checklist exist

--- products.py
+++ products.py
@@ -15,14 +15,20 @@ for p in products:
 for p in products:
 	print(p[0],'的價格是', p[1])
 
+import os #operating system
+if os.path.isfile('products.csv'): 
+	print('yeah!找到檔案')
 #讀取檔案
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品名稱,價格' in line:
-			continue #繼續
-		name, price = line. stripe().spilt(',')
-		products.append([name, price])
-print(products)
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品名稱,價格' in line:
+				continue #繼續
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案')
+
 
 #寫入檔案
 with open('products.csv', 'w' , encoding='utf-8') as f:
